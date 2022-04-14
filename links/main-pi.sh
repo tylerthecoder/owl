399529f6aaa8e5566cf632a174609c584d191de8
Tyler Tracy <tylertracy1999@gmail.com>
2022-04-14 05:01:25 +0100

    pi ngnix conf

--- links/main-pi.sh
+++ links/main-pi.sh
@@ -18,3 +18,8 @@ cd home-pi
 
 link_file .profile ~/.profile
 link_file .bashrc ~/.bashrc
+link_file nginx.conf /etc/nginx/nginx.conf
+
+go_home || exit
+link_file ./common/scripts/owl.sh /usr/local/bin/owl
+
