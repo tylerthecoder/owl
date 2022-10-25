c6664813d9b078b162efa7c8b1d09c78882613ce
Tyler Tracy <tylertracy1999@gmail.com>
2022-10-24 19:19:45 -0500

    i3 config

--- arch/scripts/arch-setup.sh
+++ arch/scripts/arch-setup.sh
@@ -1,7 +1,15 @@
-# Install Usefull Packages
-sudo pacman -S i3 i3blocks
 
-sudo pacman -Sy ranger highlight picom
+# Install Display server
+sudo pacman -S xorg-server
+
+
+# Insall window manager
+sudo pacman -S i3 i3blocks picom
+
+# Install command line utils
+sudo pacman -Sy ranger highlight
+
+
 
 sudo pacman -Sy zsh zsh-completions zsh-syntax-highlighting
 mkdir ~/.cache/zsh
@@ -10,6 +18,7 @@ mkdir ~/.cache/zsh
 #Install dev packages
 sudo pacman -Sy nodejs npm
 
+# Install dotnet
 sudo pacman -Sy dotnet-sdk dotnet-runtime aspnet-runtime
 
 npm install --global yarn
--- common/scripts/owl.sh
+++ common/scripts/owl.sh
@@ -14,7 +14,7 @@ fi
 # Install dependencies
 
 # Install fzf
-if ! command -v fzf; then
+if ! command -v fzf &> /dev/null; then
 	echo "Fzf isn't installed. Would you like to install? (y)"
 	read answer
 	if [ "$answer" = "y" ]; then
@@ -27,7 +27,7 @@ if ! command -v fzf; then
 fi
 
 # Install jq
-if ! command -v jq; then
+if ! command -v jq &> /dev/null; then
 	echo "jq isn't installed. Would you like to install? (y)"
 	read answer
 	if [ "$answer" = "y" ]; then
