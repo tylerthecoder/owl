c2ff4b2f5f065c1156e29b4368e2c5939a19e813
Tyler Tracy <tylertracy1999@gmail.com>
2023-07-25 22:22:07 -0500

    Add zsh setup script

---
+++ setups/zathura/setup.sh
@@ -0,0 +1,5 @@
+sudo pacman -S zathura zathura-pdf-mupdf
+
+owl add "$OWL_PATH/setups/zathura/zathurarc" "~/.config/zathura/zathurarc"
+
+owl link
--- setups/zsh/setup.sh
+++ setups/zsh/setup.sh
@@ -6,3 +6,5 @@ mkdir -p ~/.cache/zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ~/.config/zsh/zsh-history-substring-search
+
+owl link
