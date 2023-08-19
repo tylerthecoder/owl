64a9ab866a04fb144f74cfec0683a6ae70d9228c
Tyler Tracy <tylertracy1999@gmail.com>
2023-08-19 18:26:30 -0500

    Fix zsh history

--- common/config/.zshrc
+++ common/config/.zshrc
@@ -8,6 +8,7 @@ autoload -U colors && colors
 PS1="%B%{$fg[yellow]%}[%{$fg[red]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[yellow]%}]%{$reset_color%}$%b "
 
 # History things
+
 export HISTFILE="$XDG_STATE_HOME"/zsh/history
 HISTSIZE=100000
 SAVEHIST=100000
--- setups/zsh/setup.sh
+++ setups/zsh/setup.sh
@@ -2,6 +2,8 @@
 sudo pacman -Sy zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search
 owl add "$OWL_PATH/common/config/.zshrc" "~/.zshrc"
 mkdir -p ~/.cache/zsh
+mkdir -p $XDG_STATE_HOME/zsh
+
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-syntax-highlighting
