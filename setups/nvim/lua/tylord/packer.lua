0f4cd192edf3184062ab0c4ff3f97f8937024307
Tyler Tracy <tylertracy1999@gmail.com>
2023-09-10 19:37:51 -0500

    Add airline

--- setups/nvim/lua/tylord/packer.lua
+++ setups/nvim/lua/tylord/packer.lua
@@ -73,4 +73,6 @@ return require('packer').startup(function(use)
     use { "nvim-lua/plenary.nvim" }
     -- Smooth Scrolling
     use { 'karb94/neoscroll.nvim' }
+
+    use { 'vim-airline/vim-airline' }
 end)
