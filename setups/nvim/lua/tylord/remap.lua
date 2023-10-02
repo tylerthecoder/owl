cf598ec39d1bac4a04a0ade67f804c6206e60f0d
Tyler Tracy <17890689+tylerthecoder@users.noreply.github.com>
2023-10-02 08:21:10 -0500

    Remove unused

--- setups/nvim/lua/tylord/remap.lua
+++ setups/nvim/lua/tylord/remap.lua
@@ -1,9 +1,7 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("i", "jj", "<ESC>")
 
 -- Copy commands
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
-
