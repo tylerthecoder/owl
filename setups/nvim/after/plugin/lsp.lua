0a3cedb70292b930ac3ecbb2f8b8d800872895a7
Tyler Tracy <tylertracy1999@gmail.com>
2023-10-08 23:30:04 -0500

    Use nvim lint

--- setups/nvim/after/plugin/lsp.lua
+++ setups/nvim/after/plugin/lsp.lua
@@ -28,49 +28,17 @@ require('mason-lspconfig').setup({
     }
 })
 
-
 --=======================
 -- Configure LSPS
 --======================
 
 local lspconfig = require('lspconfig')
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-local format_sync_grp = vim.api.nvim_create_augroup("Format", {})
 
 -- Typescript setup
 lspconfig.tsserver.setup {
     capabilities = lsp_capabilities,
 }
-local function rename_file()
-    local source = vim.uri_from_bufnr(0)
-    vim.ui.input({
-        prompt = "Move to file: ",
-        completion = "file",
-        default = vim.uri_to_fname(source),
-    }, function(path)
-        print("You entered: " .. path)
-        local params = {
-            command = "_typescript.applyRenameFile",
-            arguments = { {
-                sourceUri = source,
-                targetUri = vim.uri_from_fname(path),
-            } },
-        }
-        vim.lsp.buf.execute_command(params)
-    end)
-end
-vim.keymap.set('n', '<leader>oi', rename_file)
-
--- Run eslint format on save
-lspconfig.eslint.setup({
-    capabilities = lsp_capabilities,
-    on_attach = function(client, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-        })
-    end,
-})
 
 -- Lua setup
 lspconfig.lua_ls.setup({
@@ -90,26 +58,11 @@ lspconfig.lua_ls.setup({
         }
     }
 })
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.lua",
-    callback = function()
-        vim.lsp.buf.format({ timeout_ms = 200 })
-    end,
-    group = format_sync_grp,
-})
 
 -- Rust setup
 lspconfig.rust_analyzer.setup({
     capabilities = lsp_capabilities,
 })
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.rs",
-    callback = function()
-        vim.lsp.buf.format({ timeout_ms = 200 })
-    end,
-    group = format_sync_grp,
-})
-
 
 -- Latex setup
 lspconfig.texlab.setup({
@@ -132,19 +85,11 @@ lspconfig.texlab.setup({
         }
     }
 })
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.tex",
-    callback = function()
-        vim.lsp.buf.format({ timeout_ms = 200 })
-    end,
-    group = format_sync_grp,
-})
 
--- Markdown grammar
+-- Markdown / LaTeX grammar
 lspconfig.ltex.setup({
     capabilities = lsp_capabilities,
 })
-
 
 -- C setup
 lspconfig.clangd.setup({
@@ -154,13 +99,4 @@ lspconfig.clangd.setup({
 -- Python
 lspconfig.pyright.setup({
     capabilities = lsp_capabilities,
-})
-
-require('lint').linters_by_ft = {
-    markdown = { 'vale', }
-}
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    callback = function()
-        require("lint").try_lint()
-    end,
 })
