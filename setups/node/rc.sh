757cee842f5fc4060d078f26c9982f409559397f
Tyler Tracy <tylertracy1999@gmail.com>
2024-01-10 23:17:01 -0600

    Lazy load node

--- setups/node/rc.sh
+++ setups/node/rc.sh
@@ -1,5 +1,23 @@
 export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
 
 [ -z "$NVM_DIR" ] && export NVM_DIR="$XDG_DATA_HOME"/nvm
-source /usr/share/nvm/init-nvm.sh
+
+# Make local function node-setup
+fn node-setup() {
+    echo "Sourcing node..."
+    source /usr/share/nvm/init-nvm.sh
+}
+
+fn node() {
+    unset -f node
+    node-setup
+    node "$@"
+}
+
+fn yarn() {
+    unset -f yarn
+    node-setup
+    [ ! -x "$(command -v yarn)" ] && npm install -g yarn
+    yarn "$@"
+}
 
