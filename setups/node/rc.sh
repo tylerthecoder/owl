c1a22ed16d27ab38040920aab30dcceafc733bef
Tyler Tracy <tylertracy1999@gmail.com>
2024-01-11 06:05:00 -0600

    More node setup

--- setups/node/rc.sh
+++ setups/node/rc.sh
@@ -1,4 +1,11 @@
-export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
+export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
+export NODE_REPL_HISTORY=$XDG_CONFIG_HOME/node/repl_hist
+
+mkdir -p "$XDG_CONFIG_HOME/npm"
+mkdir -p "$XDG_CONFIG_HOME/yarn"
+
+# Don't use yarn global, use bun
+# [ -x "$(command -v yarn)" ] && export PATH="$(yarn global bin):$PATH"
 
 [ -z "$NVM_DIR" ] && export NVM_DIR="$XDG_DATA_HOME"/nvm
 
@@ -18,6 +25,8 @@ fn yarn() {
     unset -f yarn
     node-setup
     [ ! -x "$(command -v yarn)" ] && npm install -g yarn
+    # IDK but this doesn't work
+    # alias yarn='yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'
     yarn "$@"
 }
 
