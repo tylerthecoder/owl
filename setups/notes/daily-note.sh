3cf1dedad8f1278b08150a2ac533c8068a709014
Tyler Tracy <tylertracy1999@gmail.com>
2024-01-29 21:13:19 -0600

    Add notes

---
+++ setups/notes/daily-note.sh
@@ -0,0 +1,18 @@
+# Open a file in the daily notes folder
+
+# Get the current date
+DATE=$(date +%Y-%m-%d)
+
+template_dir="$HOME/docs/templates"
+base_dir="$HOME/docs/notes"
+mkdir -p "$base_dir"
+
+daily_note="$base_dir/$DATE.md"
+template_note="$template_dir/Daily Note.md"
+
+# If the Daily Note template exists and there isn't already a file there, copy it over
+if [ -f "$template_note" ] && [ ! -f "$daily_note" ]; then
+    cp "$template_note" "$daily_note"
+fi
+
+nvim "$daily_note"
