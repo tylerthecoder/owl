8a61f73f4479b95b11c765ebd60b2c25e4978494
Tyler Tracy <tylertracy1999@gmail.com>
2024-04-29 20:34:22 -0500

    i3 config

--- setups/nvim/after/plugin/airline.lua
+++ setups/nvim/after/plugin/airline.lua
@@ -1,27 +1,28 @@
 vim.g.airline_mode_map = {
-    ["__"]     = '-',
-    ["c"]      = 'C',
-    ["i"]      = 'I',
-    ["ic"]     = 'I',
-    ["ix"]     = 'I',
-    ["n"]      = 'N',
-    ["multi"]  = 'M',
-    ["ni"]     = 'N',
-    ["no"]     = 'N',
-    ["R"]      = 'R',
-    ["Rv"]     = 'R',
-    ["s"]      = 'S',
-    ["S"]      = 'S',
-    ["␓"]     = 'S',
-    ["t"]      = 'T',
-    ["v"]      = 'V',
-    ["V"]      = 'V',
-    ["␖"]     = 'V',
+    ["__"] = '-',
+    ["c"] = 'C',
+    ["i"] = 'I',
+    ["ic"] = 'I',
+    ["ix"] = 'I',
+    ["n"] = 'N',
+    ["multi"] = 'M',
+    ["ni"] = 'N',
+    ["no"] = 'N',
+    ["R"] = 'R',
+    ["Rv"] = 'R',
+    ["s"] = 'S',
+    ["S"] = 'S',
+    ["␓"] = 'S',
+    ["t"] = 'T',
+    ["v"] = 'V',
+    ["V"] = 'V',
+    ["␖"] = 'V',
 }
 
 vim.g.airline_section_z = '%3l/%L:%3v'
 
+vim.g.airline_statusline_ontop = 0
+
+
 -- Hide encoding unless it's not utf-8
 vim.g['airline#parts#ffenc#skip_expected_string'] = 'utf-8[unix]'
-
-
