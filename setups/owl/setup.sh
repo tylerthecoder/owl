7dfabc2270334af35f047714c3544d42acc2e5dd
Tyler Tracy <tylertracy1999@gmail.com>
2024-09-20 21:50:19 -0700

    better setup

--- setups/owl/setup.sh
+++ setups/owl/setup.sh
@@ -1,10 +1,16 @@
 if [ ! -d ~/owl ]; then
     echo "installing owl..."
-    curl -L https://github.com/tylerthecoder/owl/releases/download/main/owl -o owl-bin
-    chmod +x owl-bin
-    mv owl-bin /usr/local/bin/owl
     git clone git@github.com:tylerthecoder/owl.git ~/owl
 else
     echo "owl already installed, updating..."
     git -C ~/owl pull
 fi
+
+# install owl if not in path
+if ! command -v owl &> /dev/null; then
+    echo "owl not in path, installing..."
+    curl -L https://github.com/tylerthecoder/owl/releases/download/main/owl -o owl-bin
+    chmod +x owl-bin
+    mv owl-bin /usr/local/bin/owl
+fi
+
