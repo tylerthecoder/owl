171a3c5fd7c285ac7280998dfe84c8620155d0c7
Tyler Tracy <tylertracy1999@gmail.com>
2025-09-08 07:55:09 -0700

    fix redwood desk

--- setups/sway/scripts/desks/redwood-sway.sh
+++ setups/sway/scripts/desks/redwood-sway.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # External big screen only at 3840x2160
 swaymsg output eDP-1 disable
-swaymsg output DP-3 enable mode 3840x2160 position 0 0
+swaymsg output DP-2 enable mode 3840x2160 position 0 0
 
 # Keyboard settings
 swaymsg input type:keyboard repeat_delay 300
